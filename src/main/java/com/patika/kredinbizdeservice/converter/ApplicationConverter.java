0f212ceec7d19a5172d97d2812aa62cb60e3c2ef
Emre Işıldaklı <suleymanemre62@gmail.com>
2024-04-09 14:29:50 +0300

    Changed converter

--- src/main/java/com/patika/kredinbizdeservice/converter/ApplicationConverter.java
+++ src/main/java/com/patika/kredinbizdeservice/converter/ApplicationConverter.java
@@ -1,6 +1,7 @@
 package com.patika.kredinbizdeservice.converter;
 
 import com.patika.kredinbizdeservice.dto.request.ApplicationRequest;
+import com.patika.kredinbizdeservice.enums.ApplicationStatus;
 import com.patika.kredinbizdeservice.model.Application;
 import com.patika.kredinbizdeservice.model.User;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,9 @@ public class ApplicationConverter {
 
     public Application toApplication(ApplicationRequest request, User user) {
         Application application = new Application();
-        application.setUser(user);
-        application.setLocalDateTime(LocalDateTime.now());
+        application.setUserId(user.getUserId());
+        application.setCreateDate(LocalDateTime.now());
+        application.setApplicationStatus(ApplicationStatus.INITIAL);
         return application;
     }
 }
